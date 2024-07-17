0fea73b735ba5b9abbfa6418ce3028bf947035b5
Kayla Rossi <kmr024@bucknell.edu>
2024-07-17 17:47:59 +0000

    inital commit

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var cors = require('cors');
 require('dotenv').config()
+const multer = require('multer')
+const upload = multer({ dest: './public/data/uploads/' })
 
 var app = express();
 
@@ -12,6 +14,17 @@ app.get('/', function (req, res) {
 });
 
 
+
+app.post('/api/fileanalyse', upload.single('upfile'), function (req, res) {
+   // req.file is the name of your file in the form above, here 'uploaded_file'
+   // req.body will hold the text fields, if there were any 
+   let responseObj=({
+    "name": req.file.originalname,
+    "type": req.file.mimetype,
+    "size": req.file.size
+   })
+   res.json(responseObj)
+});
 
 
 const port = process.env.PORT || 3000;
